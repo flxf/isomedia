33e676c9c7c4f2e41ee4db9827383ed77c142235
Felix Fung <f@vineapp.com>
2014-10-24 18:02:52 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='isomedia',
-    version='0.2',
+    version='0.2.1',
     description='This is another ISO base media format file parser.',
     author='Felix Fung',
     author_email='felix.the.cheshire.cat@gmail.com',
