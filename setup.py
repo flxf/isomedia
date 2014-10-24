264821f36246f81186c63f44c56af6d0f244ccad
Felix Fung <f@vineapp.com>
2014-10-24 15:28:44 -0400

    Update setup.py version / keywords

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='isomedia',
-    version='0.1',
+    version='0.2',
     description='This is another ISO base media format file parser.',
     author='Felix Fung',
     author_email='felix.the.cheshire.cat@gmail.com',
     url='https://github.com/flxf/isomedia',
     packages=find_packages(exclude=['tests']),
     install_requires=[],
+
+    keywords=['mp4', 'isom'],
 )
